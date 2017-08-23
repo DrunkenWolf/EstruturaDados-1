784a49b3144b05bc244f79f28cb4c54ec21bf93f
RAFAEL DOS SANTOS BRAZ <a120121@laboratorios.cct.uenp.edu.br>
2017-08-23 14:38:25 -0300

    Início do projeto 12 - ShellSort de palavras

---
+++ Metodos_Ordenacao/ShellSort_Trab_12/Funcoes.h
@@ -0,0 +1,10 @@
+#ifndef FUNCOES_H_INCLUDED
+#define FUNCOES_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#endif // FUNCOES_H_INCLUDED
---
+++ Metodos_Ordenacao/ShellSort_Trab_12/main.c
@@ -0,0 +1,14 @@
+// Nome: Rafael dos Santos Braz
+
+//RA: 120121
+
+//Trabalho 12: atividade do dia 23/08/2017, aplicação do método de ordenação ShellSort com palavras
+
+#include "Funcoes.h"
+
+int main()
+{
+
+
+    return 0;
+}
