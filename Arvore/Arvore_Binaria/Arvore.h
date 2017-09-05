eb3bd9379ba41495a7131715bf36fd3f616539b6
RAFAEL DOS SANTOS BRAZ <a120121@laboratorios.cct.uenp.edu.br>
2017-09-05 18:38:32 -0300

    Início da implementação de Árvore Binária

---
+++ Arvore/Arvore_Binaria/Arvore.h
@@ -0,0 +1,27 @@
+#ifndef ARVORE_H_INCLUDED
+#define ARVORE_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char tipo_elem;
+
+typedef struct no *pno;
+
+typedef struct no
+{
+    tipo_elem info;
+    pno esq;
+    pno dir;
+} NO;
+
+typedef pno tree;
+
+// prototipação
+
+void define(tree);
+void cria_raiz(tree, tipo_elem);
+int altura(tree);
+int numeroNos(tree);
+
+#endif // ARVORE_H_INCLUDED
